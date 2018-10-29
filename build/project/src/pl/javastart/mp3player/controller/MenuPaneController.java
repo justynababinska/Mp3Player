a2b4b71458b1aabee28f7530d53721af7f1412f4
babinskajustyna <babinskajustynaa@gmail.com>
2018-10-29 22:26:45 +0100

    Initial commit

---
+++ build/project/src/pl/javastart/mp3player/controller/MenuPaneController.java
@@ -0,0 +1,53 @@
+package pl.javastart.mp3player.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+
+public class MenuPaneController implements Initializable{
+
+	@FXML
+	private MenuBar fileMenu;
+
+	@FXML
+	private MenuItem fileMenuItem;
+
+	@FXML
+	private MenuItem dirMenuItem;
+
+	@FXML
+	private MenuItem closeMenuItem;
+
+	@FXML
+	private MenuItem aboutMenuItem;
+
+	public MenuBar getFileMenu() {
+		return fileMenu;
+	}
+
+	public MenuItem getFileMenuItem() {
+		return fileMenuItem;
+	}
+
+	public MenuItem getDirMenuItem() {
+		return dirMenuItem;
+	}
+
+	public MenuItem getCloseMenuItem() {
+		return closeMenuItem;
+	}
+
+	public MenuItem getAboutMenuItem() {
+		return aboutMenuItem;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+
+	
+}
